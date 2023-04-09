f2f2944b0d0bf0bc39d724b26d0d7a8901d7c4eb
COBALTx23 <128441331+COBALTx23@users.noreply.github.com>
2023-04-09 21:25:02 +1000

    tasks 2, 3, 4 and authentication

--- lib/comms.py
+++ lib/comms.py
@@ -2,10 +2,11 @@ import struct
 import secrets
 import hmac
 import hashlib
+import os
 
 from Crypto.Cipher import AES
 from dh import create_dh_key, calculate_dh_secret
-from lib.helpers import appendMac, macCheck, appendSalt, generate_random_string
+from lib.helpers import appendSalt
 
 class StealthConn(object):
     def __init__(self, conn, client=False, server=False, verbose=False):
@@ -14,39 +15,45 @@ class StealthConn(object):
         self.server = server
         self.verbose = True
         self.shared_secret = None
-        self.shared_iv = None
-        self.mac_key = generate_random_string()  # Generate a random key for the MAC
         self.initiate_session()
-
+        
     def initiate_session(self):
         # Perform the initial connection handshake for agreeing on a shared secret
-
-        # This can be broken into code run just on the server or just on the clientasdsad
+        
+        # Generate a one-time initialization vector (IV) for AES-OFB
+        self.shared_iv = secrets.token_bytes(16)
+        
         if self.server or self.client:
-            my_public_iv, my_private_iv = create_dh_key()
-            self.send(bytes(str(my_public_iv), 'ascii'))
-            their_public_iv = int(self.recv())
-            store = calculate_dh_secret(their_public_iv, my_private_iv)
-            self.shared_iv = store[:16]
-
             my_public_key, my_private_key = create_dh_key()
+            
             # Send them our public key
-            self.send(bytes(str(my_public_key), "ascii"))
+            self.send(my_public_key.to_bytes((my_public_key.bit_length() + 7) // 8, 'big'))
+            
             # Receive their public key
             their_public_key = int(self.recv())
+            
             # Obtain our shared secret
             self.shared_secret = calculate_dh_secret(their_public_key, my_private_key)
-            print("Shared hash: {}".format(self.shared_secret.hex()))
+            if self.verbose:
+                print("Shared hash: {}".format(self.shared_secret.hex()))
 
+            if self.verbose:
+                print("Shared IV: {}".format(self.shared_iv.hex()))
+            
     def send(self, data):
         if self.shared_secret:
             # Encrypt the message
-            cipher = AES.new(self.shared_secret, AES.MODE_OFB, iv=self.shared_iv)
+            cipher = AES.new(self.shared_secret, AES.MODE_OFB, iv=bytes(self.shared_iv))
             data_to_send = cipher.encrypt(data)
 
-            # Generate MAC and append to data
-            mac = hmac.new(self.mac_key.encode(), data_to_send, hashlib.sha256).digest()
-            data_to_send = appendMac(data_to_send, mac)
+            # Append salt to data
+            data_to_send = appendSalt(data_to_send)
+
+            # Generate HMAC
+            hmac_value = hmac.new(self.shared_secret, data_to_send, hashlib.sha256).digest()
+
+            # Append HMAC to data
+            data_to_send += hmac_value
 
             if self.verbose:
                 print()
@@ -64,27 +71,46 @@ class StealthConn(object):
 
     def recv(self):
         # Decode the data's length from an unsigned two byte int ('H')
-        pkt_len_packed = self.conn.recv(struct.calcsize("H"))
-        unpacked_contents = struct.unpack("H", pkt_len_packed)
-        pkt_len = unpacked_contents[0]
+        pkt_len_packed = b""
+        while len(pkt_len_packed) < struct.calcsize("H"):
+            pkt_len_packed += self.conn.recv(struct.calcsize("H") - len(pkt_len_packed))
+        pkt_len = struct.unpack("H", pkt_len_packed)[0]
+        
+        print("Received packet length:", pkt_len)
 
         if self.shared_secret:
             encrypted_data = self.conn.recv(pkt_len)
 
-            # Separate MAC from data
-            data_to_recv, mac = macCheck(encrypted_data)
+            print("Received encrypted data:", encrypted_data)
 
-            # Verify MAC
-            computed_mac = hmac.new(self.mac_key.encode(), data_to_recv, hashlib.sha256).digest()
-            if not hmac.compare_digest(mac, computed_mac):
-                raise ValueError("MAC does not match! Message may have been tampered with.")
+            # Separate salt from HMAC and data
+            data_to_recv, hmac_received = encrypted_data[:-32], encrypted_data[-32:]
 
+            print("Received HMAC:", hmac_received)
+            print("Received data:", data_to_recv)
+
+          # Verify HMAC
+            hmac_value = hmac.new(self.shared_secret, data_to_recv, hashlib.sha256).digest()
+            try:
+                if not hmac.compare_digest(hmac_value, hmac_received):
+                    raise ValueError("HMAC does not match! Message may have been tampered with.")
+            except ValueError as e:
+                print(str(e))
+                return None
+
+            print("HMAC check passed.")
+
+            # Remove salt from data
+            original_msg = data_to_recv[:-8]
+            
             cipher = AES.new(self.shared_secret, AES.MODE_OFB, iv=self.shared_iv)
-            original_msg = cipher.decrypt(data_to_recv)
+            original_msg = cipher.decrypt(bytes(original_msg))
+
+            print("Decrypted message:", original_msg)
 
             if self.verbose:
                 print()
-                print("Receiving message of length: {}".format(len(encrypted_data)))
+                print("Receiving message of length: {}".format(pkt_len))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
                 print("Original message: {}".format(original_msg))
                 print()
